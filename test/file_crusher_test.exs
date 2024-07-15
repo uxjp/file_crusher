064bfbdb3f802dd5ef1e320644651e4385798372
JP <38257122+uxjp@users.noreply.github.com>
2024-07-15 20:02:55 +0000

    failling test

--- test/file_crusher_test.exs
+++ test/file_crusher_test.exs
@@ -10,4 +10,9 @@ defmodule FileCrusherTest do
     assert File.exists?("./data.xls")
     assert FileCrusher.get_file_extension("./data.xls") == ".xls"
   end
+
+  test "sample test to fail and validate ci" do
+    result = 2 + 2
+    assert result == 5
+  end
 end
