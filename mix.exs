5f743a05b645cf3854e8e76e5107a1d722bd6d1b
uxjp <joaopedro1172@gmail.com>
2024-07-15 20:02:06 -0300

    update version to publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FileCrusher.MixProject do
   def project do
     [
       app: :file_crusher,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
